0ccad60e1292c4c9ae7e2cbd116cdfad2eb6bbb4
AjBorbzz <ajborbeheaddhz@yahoo.com>
2024-11-08 14:07:57 +0800

    library_management.go added switch case.

--- introduction/switch-case/library_management.go
+++ introduction/switch-case/library_management.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -50,5 +51,44 @@ func main() {
 		fmt.Println("3. Delete a book")
 		fmt.Println("4. Exit")
 		fmt.Print("Please choose an option (1-4): ")
+
+		var option int
+		_, err := fmt.Scan(&option)
+		if err != nil {
+			fmt.Println("Invalid input. Please enter a number between 1 and 4.")
+			continue
+		}
+
+		// Handle different cases using switch-case
+		switch option {
+		case 1:
+			// Add a book
+			var title, author string
+			fmt.Print("Enter the book title: ")
+			fmt.Scan(&title)
+			fmt.Print("Enter the author's name: ")
+			fmt.Scan(&author)
+			library.AddBook(title, author)
+
+		case 2:
+			// View all books
+			library.ViewBooks()
+
+		case 3:
+			// Delete a book
+			var title string
+			fmt.Print("Enter the title of the book to delete: ")
+			fmt.Scan(&title)
+			library.DeleteBook(title)
+
+		case 4:
+			// Exit the program
+			fmt.Println("Exiting Library Management System...")
+			os.Exit(0)
+
+		default:
+			// Invalid option
+			fmt.Println("Invalid option. Please choose a valid option between 1 and 4.")
+		}
 	}
 }
