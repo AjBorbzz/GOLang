dd36e40276afbaed4594d11d462b673f3e179236
AjBorbzz <ajborbeheaddhz@yahoo.com>
2024-11-08 14:17:39 +0800

    Added about page.

--- introduction/http_sample/main.go
+++ introduction/http_sample/main.go
@@ -6,10 +6,13 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello Aj!")
+	fmt.Fprintf(w, "This is the Home Page")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is the about page")
+	})
 	http.ListenAndServe(":8080", nil)
 }
