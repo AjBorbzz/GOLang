204f2dda89f24c281226fafd5f7df1cd34a7e075
AjBorbzz <ajborbeheaddhz@yahoo.com>
2024-11-08 14:40:37 +0800

    expanded to html/template

--- introduction/http_sample/main.go
+++ introduction/http_sample/main.go
@@ -2,20 +2,50 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the Home Page")
+// PageData represents the data to be passed into the HTML template
+type PageData struct {
+	Title   string
+	Heading string
+	Message string
+}
+
+// HomePageHandler renders the home page with dynamic data
+func HomePageHandler(w http.ResponseWriter, r *http.Request) {
+	// Data that will be passed to the template
+	data := PageData{
+		Title:   "My Go Website with Gorilla Mux",
+		Heading: "Welcome to My Home Page",
+		Message: "This page is dynamically rendered using Go templates and Gorilla Mux!",
+	}
+
+	// Parse the HTML template file
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		return
+	}
+
+	// Execute the template with the provided data
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+	}
 }
 
 func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/", handler)
-	router.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "This is the about page")
-	})
-	http.ListenAndServe(":8080", nil)
+	// Initialize Gorilla Mux router
+	r := mux.NewRouter()
+
+	// Define the route for the homepage
+	r.HandleFunc("/", HomePageHandler)
+
+	// Start the web server
+	fmt.Println("Server started at http://localhost:8080")
+	http.ListenAndServe(":8080", r)
 }
